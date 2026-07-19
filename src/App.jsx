3ef1f682fc6b49ca6c7b306a8e38c535778ea8f5
ViniciusAlvesofc7 <166080498+ViniciusAlvesofc7@users.noreply.github.com>
2026-07-19 16:04:06 -0300

    fix: corrigir comentário de importação do componente Depoimentos e ajustar max-width no CSS do componente Sobre

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Biblioteca from './components/Biblioteca/Biblioteca'
 import Contato from './components/Contato/Contato'
-import Depoimentos from './components/Depoimentos/Depoimentos'
+// import Depoimentos from './components/Depoimentos/Depoimentos'
 import Footer from './components/Footer/Footer'
 import Home from './components/Home/Home'
 import Servicos from './components/Servicos/Servicos'
